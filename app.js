2ef5945ac352f0e7fdb8a5fbf67eda53b00eb4ed
angelo madara <angelomadara.dev@outlook.com>
2023-03-02 11:46:29 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+// const { reset } = require('nodemon')
+
+app.use(bodyParser.urlencoded())
+
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+
+/**
+ * adding 404 page
+ */
+app.use((requset,response,next)=>{
+    response.status(404).sendFile(path.join(__dirname,'views','page-not-found.html'))
+})
+
+app.listen(9000)
---
+++ routes/admin.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const router = express.Router()
+
+router.get('/add-product',(request,response,next)=>{
+    response.sendFile(path.join(__dirname,'../','views','add-product.html'))
+})
+
+router.post('/add-product',(request,response)=>{
+    console.log(request.body)
+    response.redirect('/');
+})
+
+module.exports = router
